d34bc4d8cffcc82c8c41fac073cffa765794f2db
Shrivastava-1 <cshrivastava003@gmail.com>
2025-09-26 19:08:26 +0530

    Add Middleware

--- WebProject/urls.py
+++ WebProject/urls.py
@@ -21,6 +21,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('app1.urls')),
-    path('users/', include('users.urls')),
+    path('home/', include('app1.urls')),
+    path('', include('users.urls')),
 ]+static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
